7818ffa83941c8e1095c457c87d4c3402e1cbd9c
James Croft <jamz_c@hotmail.co.uk>
2021-09-09 15:13:36 +0100

    Reverted changes to build version retrieval

--- build/GetBuildVersion.psm1
+++ build/GetBuildVersion.psm1
@@ -7,7 +7,7 @@ Function GetBuildVersion {
     $VersionString -match "(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>\d+))?" | Out-Null
 
     if ($matches -eq $null) {
-        return "1.0.0"
+        return "1.0.0-build"
     }
 
     # Extract the build metadata

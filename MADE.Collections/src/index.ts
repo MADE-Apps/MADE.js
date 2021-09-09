7b4115b09c1b177c2a19fdf8d382f44e839da333
James Croft <jamz_c@hotmail.co.uk>
2021-09-09 15:49:31 +0100

    Added removeRange function

--- MADE.Collections/src/index.ts
+++ MADE.Collections/src/index.ts
@@ -18,17 +18,32 @@ export function update<T>(array: T[], item: T, predicate: (item: T) => boolean):
  * Makes the given destination array items equal to the items in the given source array.
  * @param {T[]} destination - The destination array to make equal to the source.
  * @param {T[]} source - The source array to provide the items.
+ * @return {T[]} The updated destination array.
  */
-export function makeEqualTo<T>(destination: T[], source: T[]) {
+export function makeEqualTo<T>(destination: T[], source: T[]): T[] {
     destination.length = 0;
     source.forEach(item => destination.push(item));
+    return destination;
 }
 
 /**
  * Adds an array of items to another.
  * @param {T[]} destination - The destination array to add items from the source to.
- * @param {T[]} source - The source array items to add.
+ * @param {T[]} itemsToAdd - The source array items to add.
+ * @return {T[]} The updated destination array.
  */
-export function addRange<T>(destination: T[], source: T[]) {
-    source.forEach(item => destination.push(item));
+export function addRange<T>(destination: T[], itemsToAdd: T[]): T[] {
+    itemsToAdd.forEach(item => destination.push(item));
+    return destination;
+}
+
+/**
+ * Removes an array of existing items from another.
+ * @param {T[]} destination - The destination array to remove items from.
+ * @param {T[]} itemsToAdd - The source array items to remove that should exist in the destination array.
+ * @return {T[]} The updated destination array.
+ */
+export function removeRange<T>(destination: T[], itemsToRemove: T[]): T[] {
+    itemsToRemove.forEach(item => destination.splice(destination.indexOf(item), 1));
+    return destination;
 }
---
+++ MADE.Collections/src/removeRange.spec.ts
@@ -0,0 +1,14 @@
+import { removeRange } from "./index";
+
+describe("when removing range of items", () => {
+    it("should remove range of items", () => {
+        // Arrange
+        const items = [1, 2, 3, 4, 5];
+
+        // Act
+        removeRange(items, [1, 2]);
+
+        // Assert
+        expect(items).toEqual([3, 4, 5]);
+    });
+});

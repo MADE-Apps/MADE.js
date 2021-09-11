f6adf7864944d0d91124c03ecc1fc78436f63dc2
James Croft <jamz_c@hotmail.co.uk>
2021-09-11 21:02:31 +0100

    Moved Chain to Collections lib

---
+++ MADE.Collections/tests/array/addRange.spec.ts
@@ -0,0 +1,14 @@
+import { addRange } from "../../src/index";
+
+describe("when adding range of items", () => {
+    it("should add range of items", () => {
+        // Arrange
+        const items = [1, 2, 3, 4, 5];
+
+        // Act
+        addRange(items, [6, 7, 8]);
+
+        // Assert
+        expect(items).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+});
